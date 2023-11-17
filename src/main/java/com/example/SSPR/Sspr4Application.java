0d1806b13c98475c3e7157ae57cd215f4a0ca4b6
RegulGrau <zzmisa73@gmail.com>
2023-11-17 12:01:05 +0400

    Initial commit

---
+++ src/main/java/com/example/SSPR/Sspr4Application.java
@@ -0,0 +1,13 @@
+package com.example.SSPR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sspr4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sspr4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SSPR/Sspr4ApplicationTests.java
@@ -0,0 +1,30 @@
+package com.example.SSPR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+
+@SpringBootTest
+class Sspr4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+
+	@Test
+	void test1() {
+		assertTrue("", 3==3);
+	}
+
+	@Test
+	void test2() {
+		assertTrue("", 3==3);
+	}
+
+	@Test
+	void test3() {
+		assertTrue("", 3==3);
+	}
+}
